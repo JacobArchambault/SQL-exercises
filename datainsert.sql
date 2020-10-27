2353586808edb5dc812dad9eaa61f744dbad8010
Jacob Archambault <jacob.archambault@waystar.com>
2020-10-27 00:10:23 -0400

    Add data insert file

---
+++ datainsert.sql
@@ -0,0 +1,2 @@
+USE NewCustomers;
+INSERT INTO CUSTOMERS values (102, "Alexandrea", "Antoinette", "123 This is my street name St.", "San Bernadino  ", "CA", "32543", "Antoinette@gmail.com", "1234567890");
